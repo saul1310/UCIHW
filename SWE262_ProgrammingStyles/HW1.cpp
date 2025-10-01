637a81c0c6a2cb04ef0d8e7b7607bce5757dda8e
Saul <saulifshin.cs@gmail.com>
2025-10-01 12:28:03 -0700

    commit

---
+++ SWE262_ProgrammingStyles/HW1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>  // Include the input/output library
+#include <string>
+
+
+
+// establish a frequency map hashtable
+// establish a string variable to hold the current string
+// find, specify file to be worked onexit
+// establish a connection into it with ifstream
+// load line by line into a string variable
+// strip of punctuation marks 
+// run each word through the frequency map
+// return frequency map
+
+
+
+
+
+int main() {
+ 
+    return 0;
+}
