2986662da7e4ff40dc01f7c428a4a7d1f7364720
Saul <saulifshin.cs@gmail.com>
2025-10-02 12:02:12 -0700

    linked list initialized for DSHW1

---
+++ SWE240P_DataStructures/HW1/HW1.py
@@ -0,0 +1,38 @@
+# Task Description:
+
+# Consider a commercial bank, Bank of Orange County, where anyone can open bank accounts. To open a bank account, the following information is needed - 
+
+# Name 
+# Address
+# Social Security
+# An initial deposit amount. 
+# The bank assigns a unique ID whenever a user opens an account. The unique ID is incrementally assigned to new users, meaning if the last new user’s ID is x, the user signing up will have a unique ID (x + 1). If a user closes their account, the unique ID can be re-claimed and re-assigned to future new users. 
+
+# Now, write a class for the Bank of Orange County to complete the following tasks - 
+
+
+
+# Task-1: Model the list of users as a linked list 
+# where each account is a node in the list.
+# Users must be sorted by their ID in the linked list. 
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+node1 = Node(15)
+node2 = Node(3)
+node3 = Node(234)
+
+node1.next = node2
+node2.next = node3
+
+head = node1
+
+current = head
+while current:
+    print(current.data, "-->")
+    current = current.next
+
+print("Null")
