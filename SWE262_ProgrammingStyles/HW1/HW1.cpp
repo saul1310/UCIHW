30b1857620120414d638407012c652f55c0824d7
Saul <saulifshin.cs@gmail.com>
2025-10-02 11:26:01 -0700

    tolower working and integrated

--- SWE262_ProgrammingStyles/HW1/HW1.cpp
+++ SWE262_ProgrammingStyles/HW1/HW1.cpp
@@ -3,12 +3,24 @@
 using namespace std;
 #include <map>
 #include <string>
+#include <cctype> 
 string test ="every day, i wonder why!";
 
+string convertToLowerCase(string& input) {
+    string lowerCase = "";
+    lowerCase.reserve(input.size()); 
 
+    for (size_t i = 0; i < input.size(); i++) {
+        lowerCase += tolower(static_cast<unsigned char>(input[i]));
+    }
+
+    return lowerCase;
+}
+
+//i should change the rawWord variable name as it takes in a line
 /* Takes in a line of text as a string, returns the line strpped of punctuation*/
 string strip(const string& input) {
-    string blacklist = ".,!-+=&?";
+    string blacklist = ".,!-+=&?'";
     string rawWord = input;
     for (int i =0; i < rawWord.length();){
         if (blacklist.find(rawWord[i]) != string::npos) 
@@ -19,10 +31,14 @@ string strip(const string& input) {
             i++;
         }
     }
-    return rawWord;
+    return convertToLowerCase(rawWord);
+    
 
 
 
+// I also need to make it convert all letters to lowercase so that it doesnt count the same word differently
+// i could insert that at the return statement of strip,
+// and just return the product of a function that converts it
 
 
 };
@@ -44,10 +60,11 @@ map<string,int> createFrequencyMap(){
 
 
     while (getline(file,line)){
-        cout << line << endl;
+
+        cout << strip(line) << endl;
 
 
-    }
+    } 
 
     file.close();
    
@@ -74,9 +91,12 @@ map<string,int> createFrequencyMap(){
 
 
 int main() {
-    string test = "hey, my !!!!!&name is joe!";
-    // createFrequencyMap();
-    cout << strip(test);
+
+    createFrequencyMap();
+
+    
  
     return 0;
 }
+
+    
