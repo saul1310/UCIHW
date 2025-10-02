c8dcde94c5b32cd034e01d25b1bc6e04bebd1fae
Saul <saulifshin.cs@gmail.com>
2025-10-01 17:05:01 -0700

    reorganized

---
+++ SWE262_ProgrammingStyles/HW1/HW1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>  // Include the input/output library
+using namespace std;
+#include <map>
+#include <string>
+
+// map<string, int> freq;
+
+
+
+// establish a frequency map hashtable
+
+map<int,string> createFrequencyMap {
+    // establish a string variable to hold the current string
+    // find, specify file to be worked onexit
+    // establish a connection into it with ifstream
+    // load line by line into a string variable
+    // strip of punctuation marks 
+    // run each word through the frequency map
+
+
+
+}
+
+
+
+
+// convert freq map into a list going from highest frequency to lowest
+// return high to low list
+
+
+
+
+int main() {
+    cout << "hey";
+ 
+    return 0;
+}
