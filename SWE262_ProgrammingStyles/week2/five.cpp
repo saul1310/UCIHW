706a15f06ba5acbf6a6027156802a658fd245b4e
Saul <saulifshin.cs@gmail.com>
2025-10-10 18:55:01 -0700

    changed main to C

---
+++ SWE262_ProgrammingStyles/week2/five.cpp
@@ -0,0 +1,10 @@
+// Recreate the freq map under these restrictions
+// Constraints:
+
+// No abstractions
+// No use of library functions
+// Possible names:
+
+// Monolith
+// Labyrinth
+// Brain dump
