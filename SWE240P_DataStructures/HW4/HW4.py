a8588466783aea8cd06b98bfb5911d9af8323a90
saul1310 <saulifshin.cs@gmail.com>
2025-10-21 16:01:49 -0700

    made hw4 folder

---
+++ SWE240P_DataStructures/HW4/HW4.py
@@ -0,0 +1,8 @@
+# You are given an input ASCII text file containing an arbitrary number of student records in the following format:
+# Operation code: 1 character ('I' for insert, 'D' for delete)
+# Student number: 7 characters
+# Student last name: 25 characters
+# Home department: 4 characters
+# Program: 4 characters
+# Year: 1 character
+# Each record is stored as one line in the text file; i.e., there is a newline character immediately following the year.
