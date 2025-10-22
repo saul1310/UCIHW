6a4a38bf7e5da0f9e49a946f0ff638f2799387d5
Saul <saulifshin.cs@gmail.com>
2025-10-21 21:43:57 -0700

    tree is made

--- SWE240P_DataStructures/HW4/HW4.py
+++ SWE240P_DataStructures/HW4/HW4.py
@@ -1,65 +1,78 @@
-# You are given an input ASCII text file containing an arbitrary number of student records in the following format:
-# Operation code: 1 character ('I' for insert, 'D' for delete)
-# Student number: 7 characters
-# Student last name: 25 characters
-# Home department: 4 characters
-# Program: 4 characters
-# Year: 1 character
-# Each record is stored as one line in the text file; i.e., there is a newline character immediately following the year.
-
-# Task 1: Build the BST
-
-#create class for student info
+# ------------------ Student Class ------------------
 class Student:
-    def __init__(self,Studentnumber,LastName,Department,Program,Year):
-        self.Studentnumber = ''
-        self.LastName = ''
-        self.Department = ''
-        self.Program = ''
-        self.year=''
+    def __init__(self, Studentnumber, LastName, Department, Program, Year):
+        self.Studentnumber = Studentnumber
+        self.LastName = LastName
+        self.Department = Department
+        self.Program = Program
+        self.Year = Year
+
+    def __str__(self):
+        return f"{self.Studentnumber} {self.LastName} {self.Department} {self.Program} {self.Year}"
 
 
-# Node class for BST
+# ------------------ Node Class ------------------
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
+
+# ------------------ BST Class ------------------
 class BST:
     def __init__(self):
-        self.root= None
+        self.root = None
+
+    def insert(self, student):
+        def _insert(root, student):
+            if not root:
+                return Node(student)
+            if student.LastName.lower() < root.data.LastName.lower():
+                root.left = _insert(root.left, student)
+            elif student.LastName.lower() > root.data.LastName.lower():
+                root.right = _insert(root.right, student)
+            return root
+        self.root = _insert(self.root, student)
+
+    def inorder(self):
+        self._inorder(self.root)
+
+    def _inorder(self, node):
+        if node:
+            self._inorder(node.left)
+            print(node.data.LastName)
+            self._inorder(node.right)
 
 
+# ------------------ Build BST from File ------------------
+def build_bst_from_file(filename):
+    bst = BST()
+    with open(filename, "r") as f:
+        for line in f:
+            if not line.strip():
+                continue
+            op = line[0]
+            student_number = line[1:8]
+            last_name = line[8:33].strip()
+            department = line[33:37]
+            program = line[37:41]
+            year = line[41]
 
-        
+            current = Student(student_number, last_name, department, program, year)
 
-    def insert(self,Student):
-        def _insert(Root,Student):
-       
-            if Root == None:
-                return Node(Student)
-            if Student.LastName.lower() < Root.Data.LastName.lower():
-                Root.left = _insert(Root.left,Student)
-            elif Student.LastName.lower > Root.Data.LastName.lower():
-                Root.right = _insert(Root.right,Student)
-            else:
-                if Student.Studentnumber < Root.Data.Studnetnumber:
-                    Root.left = _insert(Root.left,Student)
-                else:
-                    Root.right = _insert(Root.right,Student)
-            self.Root = _insert(self.Root,Student)
-      
-        
-def build_bst_from_file(file):
-        #should take in the lines with info, assign them to nodes, and also sort for each insertion
-        bst = BST
-        with open(file, "r") as file:
+            if op == 'I':
+                bst.insert(current)
+            elif op == 'D':
+                # delete method would go here
+                pass
 
-            for line in file:
-                current = Student(line[1:8:1],line[8:26:1],line[26:30:1],line[30:34:1],line[-1])
-                print(current.Department)
+            # Debug print to confirm it's reading correctly
+            # print(f"Read: {current}")
+    return bst
 
 
-var = input()
-build_bst_from_file(var)
+# ------------------ Run ------------------
+filename = input("Enter filename: ")
+tree = build_bst_from_file(filename)
+tree.inorder()
