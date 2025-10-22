846e0dae651ecfa1f21659865d6861037a69a8f1
saul1310 <saulifshin.cs@gmail.com>
2025-10-22 16:07:33 -0700

    outlined some stuff

---
+++ SWE240P_DataStructures/HW5/HW5.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class HeapBuilder:
+
+    class BST:
+        def __init__(self):
+            self.root = None 
+    
+
+            
+
