110e736c433f48e66ebd260b32efe44882d391a2
saul1310 <saulifshin.cs@gmail.com>
2025-10-23 14:54:57 -0700

    outlined problem

---
+++ SWE241P_Appl_Data_Structs/HW3.py
@@ -0,0 +1,20 @@
+#Algorithms HW3 
+#--------------------------------------------------------------------------------
+
+# Given an array of strings Strings, 
+# group the words that are anagrams to each other. You can return the answer in any order. 
+#implement the function  groupAnagram(List<String> strings) 
+# that takes a list of strings and returns a list of lists of strings
+
+#Task 1
+# -----------------------------------------
+
+def mergesort(input):
+    pass
+
+def quicksort(input):
+    pass
+
+input =  ["bucket","rat","mango","tango","ogtan","tar"]
+def groupanagram(input:list) -> list[str]:
+    pass 
